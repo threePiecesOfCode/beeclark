fd9fcc95d5abb398437ea4bf34c6ce13dd6f1515
Carlos Munoz <carlos.munoz.us03@gmail.com>
2024-06-05 13:09:50 -0400

    Added and modify some sections, remodel the designe into entire page. Add some js for set timeout in an idea about welcome alert message

--- main.js
+++ main.js
@@ -1,15 +1,68 @@
+// let words = ["SOLVING", "CREATING", "TRANSFORMING"];
+// let app = document.getElementById('typewriter');
+
+// let typewriter = new Typewriter(app, {
+//   loop: true,
+//   delay: 70,
+// });
+
+// for(let word of words){
+//   typewriter.pauseFor(100);
+//   typewriter.typeString(word);
+//   typewriter.pauseFor(800);
+//   typewriter.deleteChars(word.length);
+//     }
+// typewriter.start();
+
 let words = ["SOLVING", "CREATING", "TRANSFORMING"];
 let app = document.getElementById('typewriter');
- 
+
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 70,
 });
 
-for(let word of words){
+for (let word of words) {
   typewriter.pauseFor(100);
-  typewriter.typeString(word);
+
+  // Apply different colors based on the word
+  switch (word) {
+    case "SOLVING":
+      typewriter.typeString(`<span style="color: #a05000">${word}</span>`);
+      break;
+    case "CREATING":
+      typewriter.typeString(`<span style="color: #170075">${word}</span>`);
+      break;
+    case "TRANSFORMING":
+      typewriter.typeString(`<span style="color: black">${word}</span>`);
+      break;
+    default:
+      typewriter.typeString(word);
+  }
+
   typewriter.pauseFor(800);
   typewriter.deleteChars(word.length);
-    }
+}
+
 typewriter.start();
+
+
+// Modify last section words 
+let app2 = document.getElementById('typewriter2');
+
+let typewriter2 = new Typewriter(app2, {
+  loop: true,
+  delay: 70,
+});
+
+typewriter2
+  .pauseFor(2500)
+  .typeString('<span style="color: #a05000;">Unlock the</span> <span style="color: #170075;">Power of IT</span> and <span style="color: black;">SCADA</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+
+
+  setTimeout(function() {
+    document.querySelector('.alert').style.display = 'none';
+}, 16000); // Hide the alert after x seconds
